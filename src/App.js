ddc6dd82cf0654b45cc3ba443835e9a1991d05c9
15167 <1346181034@qq.com>
2020-11-25 17:00:48 +0800

    项目框架搭建完成

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Provider } from "react-redux";
+import { renderRoutes } from "react-router-config";
+import routes from "./router";
+import store from "./store";
+import HYAppHeader from "@/components/app-header";
+import HYAppFooter from "@/components/app-footer";
+import { HashRouter } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <HashRouter>
+          <HYAppHeader />
+          {renderRoutes(routes)}
+          <HYAppFooter />
+        </HashRouter>
+      </Provider>
+    );
+  }
 }
-
-export default App;
